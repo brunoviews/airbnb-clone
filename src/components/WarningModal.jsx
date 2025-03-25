22e433f4b31a133f28a7d716cc66e2a7c000f5e6
Harlxck <brunogustavo68@gmail.com>
2025-03-25 16:02:28 +0100

    añadido un contador por la demora en cargar el backend en Render

--- src/components/WarningModal.jsx
+++ src/components/WarningModal.jsx
@@ -18,7 +18,7 @@ const WarningModal = ({ onClose }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg w-96">
         <p className="text-center text-lg font-bold">
-          Cargando backen en <strong>Render</strong> {counter} segundos
+          Cargando backend en <strong>Render</strong> {counter} segundos
         </p>
         <button
           onClick={onClose}
